87f03e64df6f4c555ff40add293e694216563c36
sivasaikumar <sivasaikumar.pinjala@gmail.com>
2022-05-18 11:02:36 +0530

    nav bar created
    nav bar created

---
+++ Navbar/navbar.js
@@ -0,0 +1,17 @@
+const navbar = ()=>{
+    return `<div class="header">
+    <div class="left"><h2>MODEST</h2></div>
+    <div class="right">
+        <div><a href=""><span id="span_home">Home</span></a></div>
+        <div><a href="">About</a></div>
+        <div><a href="">Careers</a></div>
+        <div><a href="">Team</a></div>
+        <div><a href="">Services</a></div>
+        <div><a href="">Features</a></div>
+        <div><a href="">Contact</a></div>
+        
+    </div>
+</div>`
+}
+
+export default navbar
